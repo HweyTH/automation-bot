08787a00c40073d0e73624dc0359deb7a45b9c18
Huy Thai <thhuy1101@gmail.com>
2023-05-29 00:21:46 -0400

    Updated 2023-05-29

--- WindowsBot.py
+++ WindowsBot.py
@@ -12,7 +12,9 @@ import argparse
 
 class WindowsBot:
 
-    dir = ""
+    dir = any
+    moved = 0
+    folderCreated = []
     fileList = []
     folderList = []
     
@@ -29,9 +31,25 @@ class WindowsBot:
         print(f"Cleaning up dir: {self.dir}")
 
         return
+    
+    # a function to get lists of files and folders in directory
+    def dirList(self):
+        # get the list of all the files and folders in the directory
+        dirContent = os.listdir(self.dir)
+
+        # create a path connecting (given) directory and files' name
+        # Note: os.listdir() only supplies the names of files and folders and not the 
+        # .. directory to them
+        fullDir = [os.path.join(self.dir, doc) for doc in dirContent]
+
+        # filter files and folders into two lists
+        self.fileList = [file for file in fullDir if os.path.isfile(file)]
+        self.folderList = [folder for folder in fullDir if os.path.isdir(folder)]
+
+        return
 
     # a function to create subfolders for the correct file extensions
-    def folderCreate(files):
+    def folderCreate(self, files):
         for file in files:
             fileRoot, fileType = os.path.splitext(file)
             fileDir = os.path.dirname(fileRoot)
@@ -41,6 +59,8 @@ class WindowsBot:
             print(f"File Directory is {fileDir}")
             print(f"File Type is {fileType}")
             print("----------------------------")
+
+            break
         return    
  
 
